ae89e5d343506e74c3c215567bf41c6e7377b337
user-mosaad <msaadshahid999@gmail.com>
2026-09-13 13:34:03 +0600

    Write the code

--- MyCSharpDelegates/Program.cs
+++ MyCSharpDelegates/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿public delegate void Update();
+
+class Program
+{
+    public static void UpdateItems()
+    {
+        Console.WriteLine("Updated the item.");
+    }
+
+    public static void UpdateSales()
+    {
+        Console.WriteLine("Updated the sales.");
+    }
+
+    public static void UpdatePurchases()
+    {
+        Console.WriteLine("Updated the purchases.");
+    }
+
+    static void Main()
+    {
+        // Create an instance of the Update delegate
+        Update update = UpdateItems;
+        update += UpdateSales;
+        update += UpdatePurchases;
+
+        // Invoke the delegate
+        update();
+    }
+}
